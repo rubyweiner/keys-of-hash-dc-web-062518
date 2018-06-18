cda28ca8a936e9f50bf19715cca9294d49c7a3c7
Ruby Weiner <rubyweiner796@me.com>
2018-06-18 22:32:07 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,8 +1,8 @@
 class Hash
   def keys_of(arguments)
     array = []
-    self.each do |key, value|
-      arguments.each do |i|
+    arguments.each do |key, value|
+      value.each do |i|
         if i == value
         array.push(key)
         end
